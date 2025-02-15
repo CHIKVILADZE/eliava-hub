e665c207c073b6e51dffe884e51b5745a3b7bbd3
unknown <chikviladze555@yahoo.com>
2025-02-15 15:45:04 +0400

    first commit

---
+++ src/components/home.jsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="main">
+        <div className="section">
+          <span className="rotate">დღის</span> აქცია
+        </div>
+        <div className="section">
+          <span className="rotate">კვირის</span> აქცია
+        </div>
+        <div className="section">
+          <span className="rotate">თვის</span> აქცია
+        </div>
+      </div>
+      <div className="contactInfo">
+        <span className="phone">2 60 20 60</span>
+        <span className="deliveryText">უფასო მიტანის სერვისი</span>
+      </div>
+    </div>
+  );
+}
